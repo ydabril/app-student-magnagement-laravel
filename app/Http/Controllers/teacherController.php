852d6da43bd7b6fb115d3318ed68439f08abb9b1
ydabril <ydabrilp@ufpso.edu.co>
2023-06-14 00:04:30 -0500

    first commit

---
+++ app/Http/Controllers/teacherController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\teacher;
+use Illuminate\Http\Request;
+
+class teacherController extends Controller
+{
+    public function create(){
+        return view('createTeacher');
+    }
+    public function insert(Request $request){
+        $teacher = new teacher();
+        
+        $teacher->document = $request->document;
+        $teacher->name = $request->name;
+        $teacher->lastname = $request->lastname;
+        $teacher->phone = $request->phone;
+        $teacher->email = $request->email;
+        $teacher->address = $request->address;
+        $teacher->city = $request->city;
+        $teacher->save();
+
+        return redirect('createteacher');
+
+    }
+}
---
+++ database/migrations/2023_02_10_020448_create_student_subject_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentSubjectTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('student_subject', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedbigInteger('id_student');
+            $table->unsignedbigInteger('id_subject');
+            $table->foreign('id_student')->references('id')->on('students')->onDelete('cascade');
+            $table->foreign('id_subject')->references('id')->on('subjects')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('student_subject');
+    }
+}
